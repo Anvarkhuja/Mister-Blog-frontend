f4f7ef80457feb66e6cd15851961ef8d5a5b76cc
Anvarkhuja Bahodirkhujaev <120782827+Anvarkhuja@users.noreply.github.com>
2023-06-25 16:34:59 +0500

    Update Settings.jsx

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -16,7 +16,7 @@ export default function Settings() {
   const [success,setSuccess] = useState(false);
 
   const {user,dispatch} = useContext(Context);
-  const PF = "https://mister-blog-backend.vercel.app/api/upload"
+  const PF = "https://mister-blog-backend.vercel.app/api/upload/"
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
@@ -34,7 +34,7 @@ export default function Settings() {
       data.append("file", file);
       updatedUser.profilePic = filename;
       try{
-        await axiosInstance.post("https://mister-blog-backend.vercel.app/api/upload",data);
+        await axiosInstance.post("https://mister-blog-backend.vercel.app/api/upload/",data);
         
       }catch(err){
 
