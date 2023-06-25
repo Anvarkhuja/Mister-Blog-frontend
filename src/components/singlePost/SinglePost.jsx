4b7a19e81075995f1775160cb12903faa8151b3c
Anvarkhuja Bahodirkhujaev <120782827+Anvarkhuja@users.noreply.github.com>
2023-06-25 16:42:21 +0500

    Update SinglePost.jsx

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,7 @@ export default function SinglePost() {
   const location = useLocation()
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "https://mister-blog-backend.vercel.app/api/upload/";
+  const PF = "https://mister-blog-backend.vercel.app/images/";
   const {user} = useContext(Context);
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
